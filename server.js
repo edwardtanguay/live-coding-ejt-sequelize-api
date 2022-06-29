735b158b83b7d755b8bce7ae98a71d6a9d1c6df0
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-29 11:50:39 +0200

    route DELETE /flashcards/:id works

--- server.js
+++ server.js
@@ -6,62 +6,76 @@ const port = 3335;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.send('<h1>Flashcard API</h1>');
+    res.send('<h1>Flashcard API</h1>');
 });
 
 app.post('/flashcards', async (req, res) => {
-	const { category, front, back } = req.body;
-	try {
-		const flashcard = await Flashcard.create({ category, front, back });
-		return res.json(flashcard);
-	} catch (err) {
-		console.log(err);
-		return res.status(500);
-	}
+    const { category, front, back } = req.body;
+    try {
+        const flashcard = await Flashcard.create({ category, front, back });
+        return res.json(flashcard);
+    } catch (err) {
+        console.log(err);
+        return res.status(500);
+    }
 });
 
 app.get('/flashcards', async (req, res) => {
-	try {
-		const flashcards = await Flashcard.findAll();
-		return res.json(flashcards);
-	} catch (err) {
-		console.log(err);
-		return res.status(500);
-	}
+    try {
+        const flashcards = await Flashcard.findAll();
+        return res.json(flashcards);
+    } catch (err) {
+        console.log(err);
+        return res.status(500);
+    }
 });
 
 app.get('/flashcards/:id', async (req, res) => {
-	const id = req.params.id;
-	try {
-		const flashcard = await Flashcard.findOne({
-			where: { id }
-		});
-		return res.json(flashcard);
-	} catch (err) {
-		console.log(err);
-		return res.status(500);
-	}
+    const id = req.params.id;
+    try {
+        const flashcard = await Flashcard.findOne({
+            where: { id }
+        });
+        return res.json(flashcard);
+    } catch (err) {
+        console.log(err);
+        return res.status(500);
+    }
 });
 
 app.put('/flashcards/:id', async (req, res) => {
-	const id = req.params.id;
-	const { category, front, back } = req.body;
-	try {
-		const flashcard = await Flashcard.findOne({
-			where: { id }
-		});
-		flashcard.category = category;
-		flashcard.front = front;
-		flashcard.back = back;
-		await flashcard.save();
-		return res.json(flashcard);
-	} catch (err) {
-		console.log(err);
-		return res.status(500);
-	}
+    const id = req.params.id;
+    const { category, front, back } = req.body;
+    try {
+        const flashcard = await Flashcard.findOne({
+            where: { id }
+        });
+        flashcard.category = category;
+        flashcard.front = front;
+        flashcard.back = back;
+        await flashcard.save();
+        return res.json(flashcard);
+    } catch (err) {
+        console.log(err);
+        return res.status(500);
+    }
+});
+
+app.delete('/flashcards/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const flashcard = await Flashcard.findOne({
+            where: { id }
+        });
+		await flashcard.destroy();
+		return res.json({ message: `Flashcard with id ${id} was deleted.` });
+    } catch (err) {
+        console.log(err);
+        return res.status(500);
+    }
 });
 
 app.listen(port, async () => {
-	console.log(`listening on http://localhost:${port}`);
-	await sequelize.sync();
+    console.log(`listening on http://localhost:${port}`);
+    await sequelize.sync();
 });
