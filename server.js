ed94bf42fab587711f13690abfce49ac81f7361d
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-29 11:02:30 +0200

    added sync

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { sequelize } = require('./models');
 
 const app = express();
 const port = 3335;
@@ -7,6 +8,7 @@ app.get('/', (req, res) => {
 	res.send('<h1>Flashcard API</h1>');
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
 	console.log(`listening on http://localhost:${port}`);
+	await sequelize.sync();
 });
