bac14be6279fe09135d204f9146b92f7d6434479
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-29 11:10:53 +0200

    app.post finished

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', (req, res) => {
 	res.send('<h1>Flashcard API</h1>');
 });
 
+app.post('/flashcards', async (req, res) => {
+	const { category, front, back } = req.body;
+	try {
+		const flashcard = await Flashcard.create({ category, front, back });
+		return res.json(flashcard);
+	} catch (err) {
+		console.log(err);
+		return res.status(500);
+	}
+});
+
 app.listen(port, async () => {
 	console.log(`listening on http://localhost:${port}`);
 	await sequelize.sync();
