a8748a973c3253feb7e6441de49c1ca135268459
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-29 10:09:08 +0200

    / route finished

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3335;
 
+app.get('/', (req, res) => {
+	res.send('<h1>Flashcard API</h1>');
+});
+
 app.listen(port, () => {
 	console.log(`listening on http://localhost:${port}`);
 });
